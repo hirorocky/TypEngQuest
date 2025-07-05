df798dd12ff7b3ccebcbc88290024261f54d80e1
hirorocky <hirorocky@gmail.com>
2025-07-05 22:40:32 +0900

    fix: mac上でdiscordに通知が行かない

--- scripts/discord.sh
+++ scripts/discord.sh
@@ -14,6 +14,6 @@ fi
 DISCORD_WEBHOOK_URL=$(grep DISCORD_WEBHOOK_URL .env | cut -d '=' -f2)
 
 # メッセージをエスケープ処理
-ESCAPED_MESSAGE=$(echo -e "$MESSAGE" | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')
+ESCAPED_MESSAGE=$(echo -e "$MESSAGE" | sed 's/"/\\"/g' | awk '{printf "%s\\n", $0}' | sed 's/\\n$//')
 
 curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"${ESCAPED_MESSAGE}\"}" "${DISCORD_WEBHOOK_URL}"
